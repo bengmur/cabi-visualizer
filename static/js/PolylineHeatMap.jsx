853d8706958e0838d5e66e528808d2952fc38093
Benjamin Gmurczyk <b.gmurczyk@gmail.com>
2018-01-01 14:07:32 -0500

    Use Aphrodite for heat map styling, add new datapoints to hover infobox

--- static/js/PolylineHeatMap.jsx
+++ static/js/PolylineHeatMap.jsx
@@ -1,7 +1,10 @@
+import { StyleSheet, css } from 'aphrodite';
 import PolylineLib from '@mapbox/polyline';
 import React from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Polyline } from 'react-google-maps';
 import InfoBox from 'react-google-maps/lib/components/addons/InfoBox';
+
+import { colors } from './styles/variables';
 
 const Map = withScriptjs(withGoogleMap((props) => (
     <GoogleMap
@@ -73,7 +76,7 @@ export default class PolylineHeatMap extends React.Component {
     }
 
     render() {
-        const wrapper = <div style={{height: '100%'}} />;
+        const wrapper = <div className={css(styles.fullHeight)} />;
         return (
             <Map
                 googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${this.props.apiKey}&v=3.exp&libraries=drawing`}
@@ -85,11 +88,23 @@ export default class PolylineHeatMap extends React.Component {
             >
                 {this.state.infoBox.isVisible && (
                     <InfoBox position={this.state.infoBox.position} options={{closeBoxURL: ''}}>
-                        <div style={{backgroundColor: '#FFF', padding: '10px', whiteSpace: 'nowrap', borderRadius: '0 10px 10px 10px'}}>
-                            <div style={{userSelect: 'none'}}>
-                                <p style={{marginTop: 0}}>From: {this.state.infoBox.activePolyline.waypoints[0].name}</p>
-                                <p>To: {this.state.infoBox.activePolyline.waypoints[this.state.infoBox.activePolyline.waypoints.length - 1].name}</p>
-                                <p style={{marginBottom: 0}}>Total Trips: {this.state.infoBox.activePolyline.statistics.frequency.value}</p>
+                        <div className={css(styles.infobox)}>
+                            <div className={css(styles.noTextHighlight)}>
+                                <p className={css(styles.infoboxDatapoint)}>
+                                    <span className={css(styles.heavy)}>From:</span> {this.state.infoBox.activePolyline.waypoints[0].name}
+                                </p>
+                                <p className={css(styles.infoboxDatapoint)}>
+                                    <span className={css(styles.heavy)}>To:</span> {this.state.infoBox.activePolyline.waypoints[this.state.infoBox.activePolyline.waypoints.length - 1].name}
+                                </p>
+                                <p className={css(styles.infoboxDatapoint)}>
+                                    <span className={css(styles.heavy)}>Total Trips:</span> {this.state.infoBox.activePolyline.statistics.frequency.value}
+                                </p>
+                                <p className={css(styles.infoboxDatapoint)}>
+                                    <span className={css(styles.heavy)}>Average Time Traveled:</span> {this.state.infoBox.activePolyline.statistics.average_duration.value}
+                                </p>
+                                <p className={css(styles.infoboxDatapoint, styles.flushBottom)}>
+                                    <span className={css(styles.heavy)}>Total Time Traveled:</span> {this.state.infoBox.activePolyline.statistics.total_duration.value}
+                                </p>
                             </div>
                         </div>
                     </InfoBox>
@@ -116,3 +131,33 @@ export default class PolylineHeatMap extends React.Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    fullHeight: {
+        height: '100%',
+    },
+
+    infobox: {
+        backgroundColor: colors.white,
+        padding: '10px',
+        whiteSpace: 'nowrap',
+        borderRadius: '0 10px 10px 10px'
+    },
+
+    infoboxDatapoint: {
+        marginTop: '0',
+        marginBottom: '5px'
+    },
+
+    heavy: {
+        fontWeight: '500'
+    },
+
+    flushBottom: {
+        marginBottom: '0'
+    },
+
+    noTextHighlight: {
+        userSelect: 'none'
+    }
+});
