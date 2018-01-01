860bc0a378288c64aafdb85a209e73f84f677a83
Benjamin Gmurczyk <b.gmurczyk@gmail.com>
2018-01-01 01:00:38 -0500

    Add check that request params are not falsey when present

--- cabi_visualizer/lib/utils.py
+++ cabi_visualizer/lib/utils.py
@@ -3,5 +3,5 @@ from flask import abort
 
 def require_request_params(request_data, params):
     for param in params:
-        if param not in request_data:
+        if param not in request_data or not request_data[param]:
             abort(400)
