8468704e268c3c5030c7f348fed3d8738016bca9
Benjamin Gmurczyk <b.gmurczyk@gmail.com>
2018-01-02 22:48:30 -0500

    Tweak material card appearance

--- static/js/App.jsx
+++ static/js/App.jsx
@@ -233,7 +233,7 @@ const styles = StyleSheet.create({
     materialCard: {
         position: 'fixed',
         padding: '20px',
-        border: `1px solid ${colors.muted}`,
+        borderRadius: '2px',
         boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
         overflow: 'hidden',
         backgroundColor: colors.white
